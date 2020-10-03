0b06a4dc8dde2a94b9a0fae69457b3bbae267b8f
Darkbblue <darkbblue@outlook.com>
2020-10-03 15:05:26 +0800

    add more tools

--- Crawler.py
+++ Crawler.py
@@ -10,6 +10,7 @@ import json
 
 # ---------------------------- #
 # 根据传入的 URL 发送 GET 申请
+# 返回类型需要再取 .text 对象才是相应文本
 def get_page(url):
 	# headers 伪装
 	headers = {
@@ -34,6 +35,28 @@ def get_page(url):
 	return None # 超出重试次数后则放弃本次获取
 				# 用户可以选择进行其他处理，如跳过本页面
 
+# 根据传入的文本和正则表达式进行信息提取
+# 返回类型是一个列表
+def extract(html, exp):
+	result = re.findall(exp, html)
+	return result
+
+# 根据传入的 URL 获取图片，并根据传入的名称保存图片
+def download_pic(url, name):
+	filename = name + '.jpg' # 完成文件名
+	if os.path.exists(filename): # 跳过已下载的图片
+		return
+	pic = get_page(url).content # 图片内容
+	with open(filename, 'wb+') as f: # 写入文件
+		f.write(pic)
+
+# 根据传入的文件内容和文件名保存 json 文件
+def save_json(content, filename):
+	j_file = json.dumps(content) # 转换到 json 文件
+	with open(filename, 'w', encoding='utf-8') as f:
+		f.write(j_file)
+
+# --------------------------------- #
 
 if __name__ == '__main__':
 	html = get_page('http://www.mtime.com/top/movie/top100/').text
